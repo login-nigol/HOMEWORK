1dbde0cad0b5731bf44b98d75f7d3a5ff49f1303
Vadim <vadim.antipov@gmx.de>
2026-01-20 16:12:10 +0100

    удалён SVG

--- 33-CLOCK_DOM/js/script.js
+++ 33-CLOCK_DOM/js/script.js
@@ -1,23 +1,36 @@
-// всегда работает в strict(строгом) режиме
+'use strict';
 import { formatDateTime } from './formatDateTime.js';
 
+// 1 - константы
+// 2 - DOM селекторы
+// 3 - функции
+// 4 - навешивание обработчиков
+
+// SECOND_DEG - градус поворота секундной стрелки 6° = 360° / 60 секунд, частота секунда
+// MINUTES_DEG = 6° (частота обновления минута)
+// HOUR_DEG - градус поворота часовой стрелки 30° = 360° / 12
+// в функции опишем плавность поворота стрелки
 // константы без магии), градусы поворота за одну единицу времени
-// SECOND_DEG - градус поворота секундной стрелки 6°
-// 360° / 60 секунд = 6° одна секунда
-// MINUTES_DEG - аналагично для минут = 6° (частота обновления минута)
-// HOUR_DEG - градус поворота часовой стрелки 30°
-// 360° / 12 = 30° один час, в функции опишем плавность поворота стрелки
 const SECOND_DEG = 360 / 60; // 6°
 const MINUTE_DEG = 360 / 60; // 6°
 const HOUR_DEG = 360 / 12;   // 30°
+// коэффициент перевода градусов в радианы
+const DEG_TO_RAD = Math.PI / 180;
 
-// цепляем DOM элементы
+// DOM-селекторы: 
+// цепляем DOM элементы управления сборкой часов
+const controls = document.querySelector('#controls');
+const diametrInput = document.querySelector('#diametrInput')
+const buildBtn = document.querySelector('#buildBtn');
+// элементы часов
+const mechanicalClock = document.querySelector('#mechanical-clock');
 const secondHand = document.querySelector('.second');
 const minuteHand = document.querySelector('.minute');
 const hourHand = document.querySelector('.hour');
+const numbersWrap = document.querySelector('#numbers');
 const digitalClock = document.querySelector('#digital-clock');
 
-
+// Объявление функций:
 // основная логика
 function updateTime() {
     const timeNow = new Date();
@@ -35,9 +48,9 @@ function updateTime() {
         `translateX(-50%) rotate(${minute * MINUTE_DEG}deg)`;
     hourHand.style.transform =
         `translateX(-50%) rotate(${(hour + minute / 60) * HOUR_DEG}deg)`;
-    // (hour + minute / 60) * HOUR_DEG - плавность
-    // minute / 60 - доля часа
     // hour + minute/60 - реальное положение часовой стрелки между делениями
+    // minute / 60 - доля часа
+    // ( ... ) * HOUR_DEG - плавность
     // стрелка двигается не скачками раз в час, а плавно смещается каждую минуту
 
     // обновляем цифровое время на циферблате
@@ -47,109 +60,110 @@ function updateTime() {
         String(second).padStart(2, '0');
 }
 
+// коофиценты для разметки цифр от радиуса циферблата
+const NUMBERS_RADIUS_K = 0.85;  // позиционирование от центра
+const NUM_CIRCLE_DIAM_K = 0.22; // размер кружков цифр
+const NUM_FONT_SIZE_K = 0.13;   // размер цифр
+const NUM_TEXT_OFFSET_K = 0.05; // вертикальный микросдвиг цифр
 
-// цепляем элементы управления сборкой часов
-const controls = document.querySelector('#controls');
-const diametrInput = document.querySelector('#diametrInput')
-const buildBtn = document.querySelector('#buildBtn');
+// Функция-обработчик:
+// встраиваю DOM-цифры в сборку часов
+function buildDomNumbers() {
+    const rect = mechanicalClock.getBoundingClientRect();
 
-// цепляем механические часы
-const mechanicalClock = document.querySelector('#clock');
+    // центр в часах
+    const clockCenterX = rect.width / 2;
+    const clockCenterY = rect.height / 2;
+    const clockRadius = rect.width / 2;
 
-// SVG с цифрами (файл через <object>) — с ним можно работать только после load
-const clockNumberObj = document.querySelector('#clockNumber'); // <object>
-const SVG_NS = 'http://www.w3.org/2000/svg';
+    // радиус размещения цифр и диаметр кружка цифры
+    const numbersR = clockRadius * NUMBERS_RADIUS_K;
+    const circleD = clockRadius * NUM_CIRCLE_DIAM_K;
 
-// координатная система viewBox 0..100, центр в (50,50)
-const SVG_CENTER_X = 50;
-const SVG_CENTER_Y = 50;
-const NUMBERS_RADIUS  = 41;       // радиус размещения цифр (под окружность)
-const NUMBERS_CIRCLE_RADIUS = 6;  // радиус кружка под цифрой
-const TEXT_OFFSET_Y = '0.1em';    // микросдвиг текста в кружке вниз
+    numbersWrap.innerHTML = '';
 
+    // 12 часов в обороте
+    for (let hour=1; hour<=12; hour++) {
+        // hour * 30° -> переводим градусы в радианы
+        const angleRad = hour * HOUR_DEG * DEG_TO_RAD;
 
-// создаём цифры 1-12 в кружках внутри SVG
-function buildSvgNumbers() {
-    const svgDoc = clockNumberObj.contentDocument; // доступ к DOM внутри svg-файла
-    const numbersGroup = svgDoc.querySelector('#numbers'); // <g id="numbers"></g> в svg   
+        // координаты центра цифры по окружности
+        const numCenterX = clockCenterX + numbersR * Math.sin(angleRad);
+        const numCenterY = clockCenterY - numbersR * Math.cos(angleRad);
 
-    // на случай повторной сборки — чистим группу
-    numbersGroup.innerHTML = '';    
-    
-    for ( let hour =1; hour <=12; hour++ ) {
-        const angleRad = (hour * HOUR_DEG) * Math.PI / 180; // градусы -> радианы
-        
-        // x через sin, y через -cos (чтобы 12 было сверху)
-        const x = SVG_CENTER_X + NUMBERS_RADIUS * Math.sin(angleRad);
-        const y = SVG_CENTER_Y - NUMBERS_RADIUS * Math.cos(angleRad);
-        
-        // кружок под цифрой
-        const c = svgDoc.createElementNS(SVG_NS, 'circle');
-        c.setAttribute('class', 'num-circle');
-        c.setAttribute('cx', x);
-        c.setAttribute('cy', y);
-        c.setAttribute('r', NUMBERS_CIRCLE_RADIUS);
-        numbersGroup.appendChild(c);
-        
-        // цифра в кружке
-        const t = svgDoc.createElementNS(SVG_NS, 'text');
-        t.setAttribute('class', 'num-text');
-        t.textContent = hour;
-        t.setAttribute('x', x);
-        t.setAttribute('y', y);
-        t.setAttribute('dy', TEXT_OFFSET_Y);
-        numbersGroup.appendChild(t);
-    }
+        // создаём DOM-элемент цифры
+        const numberEl = document.createElement('div');
+        numberEl.className = 'clock-num';
+        numberEl.textContent = hour;
+        numberEl.style.lineHeight = '1';
+        numberEl.style.paddingTop = `${clockRadius * NUM_TEXT_OFFSET_K}px`;
+
+        // размеры кружка
+        numberEl.style.width = `${circleD}px`;
+        numberEl.style.height = `${circleD}px`;
+
+        // позиционирую цифру по её центру
+        numberEl.style.left = `${numCenterX}px`;
+        numberEl.style.top = `${numCenterY}px`;
+        numberEl.style.transform = 'translate(-50%, -50%)';
+
+        // размер шрифта от радиуса часов
+        numberEl.style.fontSize = `${clockRadius * NUM_FONT_SIZE_K}px`;
+
+        numbersWrap.appendChild(numberEl);
+    }    
 }
+// buildDomNumbers();
 
-// 
+// диапозон размера диаметра циферблата от 12 до 50 (em)
+const MIN_DIAMETER = 200;
+const MAX_DIAMETER = 800;
+const TIMER_INTERVAL = 1000; // в милисекундах
 
-// ждём пока свг подгрузится
-clockNumberObj.addEventListener('load', buildSvgNumbers);
+let timerId = null; // чтобы не запускать часы несколько раз
+
+// Обработчики событий:
+// обработчик события клика кнопки
+buildBtn.addEventListener('click', ()=> {
+    const diameter = Number(diametrInput.value);
+    
+    if ( diameter < MIN_DIAMETER || diameter > MAX_DIAMETER ) {
+        alert('Введите число от 200 до 800!')
+        return;
+    }
+    
+    // применяем диаметр циферблата
+    mechanicalClock.style.width = `${diameter}px`;
+    mechanicalClock.style.height = `${diameter}px`;
+    
+    // прячем управление, показываем часы
+    controls.style.display = 'none';
+    mechanicalClock.hidden = false;
+
+    // даём браузеру применить размеры, потом строим цифры
+    requestAnimationFrame(()=>{
+        buildDomNumbers();
+    });
+    
+    // защита от повторного сбора часов
+    if ( timerId ) clearInterval( timerId );    
+    
+    // сборка часов: сразу выставляем текущее время + интервал обновления
+    updateTime();
+    buildDomNumbers();
+    timerId = setInterval(updateTime, TIMER_INTERVAL);
+});
+// опционально
+// buildDomNumbers();
+// timerId = setInterval(updateTime, TIMER_INTERVAL);
 
 // обработчик события клавиатуры
 diametrInput.addEventListener('keydown', (e) => {
     if (e.key === 'Enter' || e.key === ' ') {
         e.preventDefault(); // чтобы не было лишних действий
-        buildBtn.click();   // запускаем сборку
+        buildBtn.click();
     }
 });
-
-// диапозон размера диаметра циферблата от 12 до 50 (em)
-const MIN_DIAMETER = 12;
-const MAX_DIAMETER = 50;
-const TIMER_INTERVAL = 1000; // в милисекундах
-
-// чтобы не запускать часы несколько раз
-let timerId = null;
-
-// обработчик события клика кнопки «Собрать часы»
-buildBtn.addEventListener('click', ()=> {
-    const diameter = Number(diametrInput.value);
-    
-    if ( diameter < MIN_DIAMETER || diameter > MAX_DIAMETER ) {
-        alert('Введите число от 12 до 50!')
-        return;
-    }
-    
-    // применяем диаметр циферблата
-    mechanicalClock.style.width = `${diameter}em`;
-    mechanicalClock.style.height = `${diameter}em`;
-    
-    // прячем управление, показываем часы
-    controls.style.display = 'none';
-    mechanicalClock.hidden = false; 
-
-    // защита от повторного сбора часов
-    if ( timerId ) clearInterval( timerId );
-
-    // сборка часов: сразу выставляем текущее время + интервал обновления
-    updateTime();
-    timerId = setInterval(updateTime, TIMER_INTERVAL);
-    
-});
-
-
 
 // === проверка вращения === //
 // let angleSec = 0;
